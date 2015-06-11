635586ca9f7b7ca8e4694cfa9ce52a405e083b0a
crossbizz <crossbizz@gmail.com>
2015-06-11 13:56:34 -0700

    Update Main.java

--- src/main/java/com/EmailServiceAbstraction/Main.java
+++ src/main/java/com/EmailServiceAbstraction/Main.java
@@ -1,8 +1,14 @@
+package com.EmailServiceAbstraction;
+
 import java.io.IOException;
 import javax.ws.rs.core.UriBuilder;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import com.sun.grizzly.http.SelectorThread;
 
 public class Main {
     private static java.net.URI getBaseURI(String hostname, int port) {
@@ -11,8 +17,13 @@ public class Main {
 
     protected static org.glassfish.grizzly.http.server.HttpServer startServer(java.net.URI uri) throws IOException {
         System.out.println("Starting grizzly...");
-        ResourceConfig rc = new PackagesResourceConfig("com.EmailServiceAbstraction.resources");
-        return GrizzlyServerFactory.createHttpServer(uri, rc);
+        
+        final Map<String, String> initParams = new HashMap<String, String>();
+
+        initParams.put("com.sun.jersey.config.property.packages","package com.EmailServiceAbstraction.resources;");
+        initParams.put("com.sun.jersey.config.property.packages","package com.EmailServiceAbstraction.models;");
+        
+        return GrizzlyServerFactory.createHttpServer(uri, initParams);
     }
 
     public static void main(String[] args) throws IOException {
