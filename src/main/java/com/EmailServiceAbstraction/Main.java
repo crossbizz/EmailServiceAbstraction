57f95729671863c5f42f2df4fbaa09f5f999c8c9
crossbizz <crossbizz@gmail.com>
2015-06-11 13:59:50 -0700

    Update Main.java

--- src/main/java/com/EmailServiceAbstraction/Main.java
+++ src/main/java/com/EmailServiceAbstraction/Main.java
@@ -1,39 +1,21 @@
-package com.EmailServiceAbstraction;
-
-import java.io.IOException;
-import javax.ws.rs.core.UriBuilder;
-import com.sun.jersey.api.core.PackagesResourceConfig;
-import com.sun.jersey.api.core.ResourceConfig;
-import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 public class Main {
-    private static java.net.URI getBaseURI(String hostname, int port) {
-        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
-    }
-
-    protected static org.glassfish.grizzly.http.server.HttpServer startServer(java.net.URI uri) throws IOException {
-        System.out.println("Starting grizzly...");
+    public static void main(String[] args) throws IOException {
         
+        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
         final Map<String, String> initParams = new HashMap<String, String>();
 
         initParams.put("com.sun.jersey.config.property.packages","package com.EmailServiceAbstraction.resources;");
         initParams.put("com.sun.jersey.config.property.packages","package com.EmailServiceAbstraction.models;");
         
-        return GrizzlyServerFactory.createHttpServer(uri, initParams);
-    }
-
-    public static void main(String[] args) throws IOException {
-        java.net.URI uri = getBaseURI(System.getenv("HOSTNAME"), Integer.valueOf(System.getenv("PORT")));
-        org.glassfish.grizzly.http.server.HttpServer httpServer = startServer(uri);
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nTry out %shelloworld\nHit enter to stop it...",
-                uri, uri));
-        while(true) {
-            System.in.read();
-        }
+        System.out.println("Starting grizzly...");
+        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
+        System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
     }
 }
