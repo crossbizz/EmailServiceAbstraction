f855985a65bdbd90fc9170aa06722520b22321f2
Manish Maheshwari <crossbizz@gmail.com>
2015-06-11 20:43:26 -0700

    keep server running3

--- src/main/java/com/EmailServiceAbstraction/Main.java
+++ src/main/java/com/EmailServiceAbstraction/Main.java
@@ -14,22 +14,37 @@ import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 
 public class Main {
-	 private static URI getBaseURI(String hostname, int port) {
+	 
+	/**
+	 * Get the Base Uri of the box
+	 * @param hostname
+	 * @param port
+	 * @return
+	 */
+	private static URI getBaseURI(String hostname, int port) {
 	        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
 	    }
 	 
+	 /**
+	  * Get the Server
+	  * @param uri
+	  * @return
+	  * @throws IOException
+	  */
 	 protected static HttpServer GetServer(URI uri) throws IOException {
 	        System.out.println("Starting grizzly...");
 	        ResourceConfig rc = new PackagesResourceConfig("com.EmailServiceAbstraction.resources");
 	        return GrizzlyServerFactory.createHttpServer(uri, rc);
 	    }
 	 
-    public static void main(String[] args) throws IOException {
-        
-        final Map<String, String> initParams = new HashMap<String, String>();
-        ResourceConfig rc = new PackagesResourceConfig("com.EmailServiceAbstraction.resources");
+    /**
+     * Start the Grizzly Server
+     * @param args
+     * @throws IOException
+     */
+	 public static void main(String[] args) throws IOException {
         try {
-            System.out.println("Starting grizzly...");
+            System.out.println("Starting grizzly ...");
             URI uri = getBaseURI(System.getenv("HOSTNAME"), Integer.valueOf(System.getenv("PORT")));
             HttpServer httpServer = GetServer(uri);
             httpServer.start();
