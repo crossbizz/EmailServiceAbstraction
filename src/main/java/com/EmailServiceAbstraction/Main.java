5894cd792d55e422c0beb459858294e100bb0003
Manish Maheshwari <crossbizz@gmail.com>
2015-06-11 20:37:51 -0700

    heroku env vars

--- src/main/java/com/EmailServiceAbstraction/Main.java
+++ src/main/java/com/EmailServiceAbstraction/Main.java
@@ -5,22 +5,34 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.core.UriBuilder;
+
 import org.glassfish.grizzly.http.server.HttpServer;
+
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 
 public class Main {
+	 private static URI getBaseURI(String hostname, int port) {
+	        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+	    }
+	 
+	 protected static HttpServer GetServer(URI uri) throws IOException {
+	        System.out.println("Starting grizzly...");
+	        ResourceConfig rc = new PackagesResourceConfig("com.EmailServiceAbstraction.resources");
+	        return GrizzlyServerFactory.createHttpServer(uri, rc);
+	    }
+	 
     public static void main(String[] args) throws IOException {
         
         final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
         final Map<String, String> initParams = new HashMap<String, String>();
-        initParams.put("com.sun.jersey.config.property.packages","com.EmailServiceAbstraction.resources");
         ResourceConfig rc = new PackagesResourceConfig("com.EmailServiceAbstraction.resources");
         try {
             System.out.println("Starting grizzly...");
             URI uri = URI.create(baseUri);
-            HttpServer httpServer = GrizzlyServerFactory.createHttpServer(uri, rc);
+            HttpServer httpServer = GetServer(uri);
             httpServer.start();
             System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
             System.in.read();
